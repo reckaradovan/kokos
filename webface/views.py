4b439bc1041539649fbd5f2ff4d0532c617c3fa8
Radovan Rečka <rec34137@spseol.cz>
2017-12-04 12:11:37 +0100

    1 část

--- webface/views.py
+++ webface/views.py
@@ -19,10 +19,35 @@ def super():
     return render_template('super.html', text=text)
 
 
-@app.route('/number/<i>')
-def number(i):
-    return render_template('number.html', number=i)
+@app.route('/login/', methods=['POST'])
+def login_post():
+    jmeno = request.form.get('jmeno')
+    heslo = request.form.get('heslo')
+    next = request.args.get('next')
+    if check_password_hash(str(pwhash.get(jmeno)),heslo):
+        session['jmeno'] = jmeno
+        flash('Úspěšně jsi se přihlásil.', 'zelena')
+        return redirect(next or url_for('index'))
+    else:
+        flash('chybné jméno nebo heslo', 'cervena')
+        if next:
+            return redirect(url_for('login', next=next))
+        else:
+            return redirect(url_for('login'))
+        
+@app.route('/login/', methods=['GET'])
+def login():
+    return render_template('login.html')
+pwhash = {}
+pwhash['radovan'] = 'pbkdf2:sha1:1000$Kc43JenL$9069898de683c75335adfde347d990d111333fc0'
+pwhash['tonda'] = 'pbkdf2:sha1:1000$XrXA9w3K$cb82663aaa944100ef63928530482e6e122bf365'
+pwhash['marek'] = 'pbkdf2:sha1:1000$ca4ebdux$53056bc9ebe8dca3662f0421a0b9dcc2edd7f4f0'
 
+@app.route('/logout/', methods=['GET'])
+def logout():
+    session.pop('jmeno', None)
+    return redirect(url_for('login'))
+    
 
 @app.errorhandler(404)
 def page_not_found(error):
